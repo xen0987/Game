1942d02b01e65f7ea63f74efed1c57b25a7f2ad2
Vasj0001 <qfilin@mail.ru>
2014-09-12 18:36:15 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,20 @@
+#include <iostream> // cout
+#include <stdlib.h> //  clrscr()
+#include <stdio.h> // gets()
+#include <conio.h> // getch()
+#include <string.h> 
+
+
+using namespace std;
+
+
+
+int main()
+{
+	int mas[10];
+	mas[0]=1;
+	cout<<mas;
+	
+	return 0;
+	
+}
